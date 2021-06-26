05c80978c786f01f07f1f8e71382e1002501c5a7
ReeceDeveloper <ReeceDev03@gmail.com>
2021-06-25 22:24:25 -0400

    Finish v1.0.0 of the Essentials plugin & add the README.md.

--- Plugins/Essentials/Essentials.cs
+++ Plugins/Essentials/Essentials.cs
@@ -17,7 +17,9 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using Oxide.Core.Libraries.Covalence;
 
 namespace Oxide.Plugins
 {
@@ -26,10 +28,30 @@ namespace Oxide.Plugins
 
     public class Essentials : CovalencePlugin
     {
+        #region Configuration
+        
         private class PluginConfig
         {
-            [JsonProperty("(1). Enable the Essentials plugin?")]
+            [JsonProperty("(1). Enable the Essentials plugin?")] 
             public bool PluginEnabled { get; private set; } = true;
+
+            [JsonProperty("(2). Enable the server's whitelist?")] 
+            public bool WhitelistEnabled { get; private set; }
+            
+            [JsonProperty("(3). Announce when the server is saved?")]
+            public bool AnnounceWorldSave { get; private set; }
+            
+            [JsonProperty("(4). Disable F1-give messages?")]
+            public bool DisableGiveMessages { get; private set; }
+
+            [JsonProperty("(5). Announce when a player joins the server?")]
+            public bool AnnounceJoinEvents { get; private set; } = true;
+
+            [JsonProperty("(6). Announce when a player leaves the server?")]
+            public bool AnnounceLeaveEvents { get; private set; } = true;
+            
+            [JsonProperty("(x). Enable debug statements in console?")]
+            public bool DebugEnabled { get; private set; }
         }
         
         private PluginConfig _config;
@@ -39,19 +61,139 @@ namespace Oxide.Plugins
             Config.WriteObject(new PluginConfig(), true);
         }
 
-        private new void SaveConfig()
-        {
-            Config.WriteObject(_config, true);
-        }
+        #endregion Configuration
 
+        #region Localisation
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["WhitelistDenyEvent"] = "You are not whitelisted on this server.",
+                ["WorldSaveEvent"] = "The server is now saving.",
+                ["PlayerJoinEvent"] = "{0} has joined the server!",
+                ["PlayerLeaveEvent"] = "{0} has left the server!"
+            }, this);
+        }
+        
+        #endregion Localisation
+        
         private void Init()
         {
             _config = Config.ReadObject<PluginConfig>();
 
             if (!_config.PluginEnabled)
             {
-                
+                Puts("- Currently disabled via the configuration file.");
+                return;
             }
+            
+            if(_config.WhitelistEnabled)
+                InitWhitelist();
+            
+            Puts("- Successfully been initialised.");
         }
+
+        private void Unload()
+        {
+            Puts(" - Successfully been unloaded.");
+        }
+        
+        #region Whitelisting
+
+        private const string WhitelistPerm = "essentials.whitelist.allow";
+
+        private void InitWhitelist()
+        {
+            if (_config.DebugEnabled) 
+                Puts("- Whitelisting is currently enabled.");
+
+            permission.RegisterPermission(WhitelistPerm, this);
+        }
+        
+        bool IsWhitelisted(string id)
+        {
+            var player = players.FindPlayerById(id);
+
+            return player != null && permission.UserHasPermission(id, WhitelistPerm);
+        }
+
+        object CanUserLogin(string name, string id)
+        {
+            if (!_config.WhitelistEnabled)
+                return null;
+
+            if (IsWhitelisted(id))
+                return null;
+
+            if (_config.DebugEnabled)
+                Puts($"- {name} tried to connect but was not whitelisted.");
+
+            return lang.GetMessage("WhitelistDenyEvent", this);
+        }
+
+        #endregion Whitelisting
+        
+        #region SaveAnnounce
+
+        void OnServerSave()
+        {
+            if (!_config.AnnounceWorldSave)
+                return;
+            
+            if (_config.DebugEnabled)
+                Puts("- Announcing that the world is saving.");
+            
+            server.Broadcast(lang.GetMessage("WorldSaveEvent", this));
+            
+        }
+        
+        #endregion SaveAnnounce
+
+        #region GiveMessages
+
+        object OnServerMessage(string message, string name)
+        {
+            if (!_config.DisableGiveMessages)
+                return null;
+
+            if (!name.Equals("SERVER") && message.Contains("gave"))
+                return null;
+            
+            if (_config.DebugEnabled)
+                Puts("- Blocked a server F1 give message.");
+
+            return true;
+        }
+        
+        #endregion GiveMessages
+
+        #region PlayerEvents
+
+        void OnUserConnected(IPlayer player)
+        {
+            if (!_config.AnnounceJoinEvents)
+                return;
+            
+            if(_config.DebugEnabled)
+                Puts($"- {player.Name} has joined the server.");
+
+            var message = lang.GetMessage("PlayerJoinEvent", this);
+            server.Broadcast(string.Format(message, player.Name));
+        }
+
+        void OnUserDisconnected(IPlayer player)
+        {
+            if (!_config.AnnounceLeaveEvents)
+                return;
+            
+            if(_config.DebugEnabled)
+                Puts($"- {player.Name} has left the server.");
+
+            var message = lang.GetMessage("PlayerLeaveEvent", this);
+            server.Broadcast(string.Format(message, player.Name));
+        }
+
+        #endregion PlayerEvents
     }
 }
---
+++ Plugins/Essentials/README.md
@@ -0,0 +1,72 @@
+# Essentials
+
+The Essentials plugin provides extra functions to improve quality of life while playing Rust.
+All functions within the plugin (including the messages) are configurable by the end-user.
+
+## Default (Plugin) Configuration
+
+Below is the default plugin configuration settings should you need to reset your configuration.
+
+```json
+{
+  "(1). Enable the Essentials plugin?": true,
+  "(2). Enable the server's whitelist?": false,
+  "(3). Announce when the server is saved?": false,
+  "(4). Disable F1-give messages?": false,
+  "(5). Announce when a player joins the server?": true,
+  "(6). Announce when a player leaves the server?": true,
+  "(x). Enable debug statements in console?": false
+}
+```
+
+## Default (Language) Configuration
+
+Below is the default language configuration settings should you need to reset your language file.
+
+```json
+{
+  "WhitelistDenyEvent": "You are not whitelisted on this server.",
+  "WorldSaveEvent": "The server is now saving.",
+  "PlayerJoinEvent": "{0} has joined the server!",
+  "PlayerLeaveEvent": "{0} has left the server!"
+}
+```
+
+To use colour codes use `[#HEXCODE]Text[/#]` to do such; for example, `[#123ABC]Text[/#]`.
+
+## Dependencies
+
+* [Knowledge of uMod](https://umod.org/documentation/api/overview)
+* [Knowledge of C#](https://duckduckgo.com/?q=Learn+C%23&t=ffab&ia=web)
+* [uMod](https://umod.org/)
+
+## Links & Information
+
+* [YouTube Channel](https://www.youtube.com/channel/UC1h5k4wUa9VRpZpH2bqr3kg)
+* [GitHub Account](https://github.com/ReeceDeveloper)
+* [GitLab Account](https://gitlab.com/ReeceDeveloper)
+* [Discord Server](https://discord.com/)
+
+## Contact Me!
+
+You can contact me on Discord by adding `Reece#9545` or by joining my [Discord Server](https://discord.com/).
+You can also try messaging me on [Twitter](https://twitter.com/ReeceDeveloper). I also have
+[Signal](https://signal.org) and will add people by request.
+
+
+## MIT License
+
+Copyright © 2021 ReeceDeveloper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
